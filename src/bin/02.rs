388beec5acf7bed875da4d26e2cc82cbd85fd4c1
Braidon <braidon@superapi.com.au>
2025-12-03 13:21:35 +1100

    day2 p2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -13,7 +13,29 @@ fn is_invalid(id: u64) -> bool {
 
 fn is_invalid_p2(id: u64) -> bool {
     // figure it out later :shrug:
-    false
+    let id_str = id.to_string();
+    let id_str_length = id_str.len();
+
+    // The factors of the length of id_str
+    let factors = (1..=id_str_length / 2)
+        .filter(|&x| id_str_length % x == 0)
+        .collect::<Vec<_>>();
+
+    for factor in factors {
+        // parts is the substrings of id_str of length factor
+        let parts = id_str
+            .chars()
+            .collect::<Vec<char>>()
+            .chunks(factor)
+            .map(|chunk| chunk.iter().collect::<String>())
+            .collect::<Vec<String>>();
+
+        // Check if all parts are equal
+        if parts.iter().all(|part| part == &parts[0]) {
+            return true;
+        }
+    }
+    return false;
 }
 
 pub fn part_one(input: &str) -> Option<u64> {
@@ -39,7 +61,25 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let ranges = input.split(',').map(|range| {
+        let parts: Vec<u64> = range
+            .trim()
+            .split('-')
+            .map(|s| s.parse().unwrap())
+            .collect();
+        (parts[0], parts[1])
+    });
+    let mut count = 0;
+
+    for (from, to) in ranges {
+        for id in from..=to {
+            if is_invalid_p2(id) {
+                count += id;
+            }
+        }
+    }
+
+    Some(count as u64)
 }
 
 #[cfg(test)]
@@ -55,6 +95,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4174379265));
     }
 }
